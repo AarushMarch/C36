5a8052e8d5e19295bbfbd7e43b4ce329ba115f48
AarushMarch <74696156+AarushMarch@users.noreply.github.com>
2021-02-11 22:39:00 +1300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,15 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement("h2");
+        title.html("carRacingGame");
+        title.position(130, 0);
+        var input = createInput("name");
+        var button =createButton("Play");
+        var greeting = createElement("h3");
+        input.position(130, 160);
+        button.position(250, 160);
+    }
+}
